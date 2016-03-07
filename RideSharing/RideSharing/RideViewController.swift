ae30f449de38dc7664499028d9febb0a65e7be7e
Tarang Khanna <khanna17@purdue.edu>
2016-03-07 16:22:44 -0500

    menu controller added

---
+++ RideSharing/RideSharing/RideViewController.swift
@@ -0,0 +1,45 @@
+//
+//  RideViewController.swift
+//  RideSharing
+//
+//  Created by Tarang khanna on 3/7/16.
+//  Copyright © 2016 Tarang khanna. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class RideViewController: UIViewController {
+    
+    
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if self.revealViewController() != nil {
+            menuButton.target = self.revealViewController()
+            menuButton.action = "revealToggle:"
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
