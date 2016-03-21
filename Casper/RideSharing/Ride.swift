e38aea3f93f66269cf482b7d63a07f640896a3c9
akshatgyl <akshatgyl@gmail.com>
2016-03-20 21:07:59 -0400

    added Ride model

--- Casper/RideSharing/Ride.swift
+++ Casper/RideSharing/Ride.swift
@@ -6,4 +6,26 @@
 //  Copyright © 2016 Tarang khanna. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import Parse
+
+
+class Ride : NSObject {
+    
+    class func postRide (departurePoint: String?, arrivalPoint: String?, dateAndTime: NSDate?, price: String?, description: String?, availability: Bool?, withCompletion completion: PFBooleanResultBlock? ) {
+        let ride = PFObject(className: "Ride")
+        
+        ride["departurePoint"] = departurePoint
+        ride["arrivalPoint"] = arrivalPoint
+        ride["dateAndTime"] = dateAndTime
+        ride["price"] = price
+        ride["description"] = description
+        ride["availability"] = availability
+        ride["driver"] = PFUser.currentUser()
+        
+        ride.saveInBackgroundWithBlock(completion)
+    }
+    
+    
+    
+}
--- Casper/RideSharing/driveViewController.swift
+++ Casper/RideSharing/driveViewController.swift
@@ -39,5 +39,28 @@ class driveViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    @IBOutlet weak var availSwitch: UISwitch!
+    @IBOutlet weak var desTxt: UITextField!
+    @IBOutlet weak var priceTxt: UITextField!
+    @IBOutlet weak var dateView: UIDatePicker!
+    @IBOutlet weak var toTxt: UITextField!
+    @IBOutlet weak var fronTxt: UITextField!
 
+    @IBAction func onPost(sender: AnyObject) {
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = "MMM d, yyyy, HH:mm"
+        let createdString = "\(dateView.date)"
+        print(createdString)
+        let date = formatter.dateFromString(createdString)
+        print(date)
+        
+        Ride.postRide(fromTxt.text, arrivalPoint: toTxt.text, dateAndTime: dateView.date, price: priceTxt.text, description: desTxt.text, availability: true) { (success: Bool, error: NSError?) -> Void in
+            if (success) {
+                print("success")
+            }
+            if error != nil {
+                print(error)
+            }
+        }
+    }
 }
