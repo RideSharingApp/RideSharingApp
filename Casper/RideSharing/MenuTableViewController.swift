dffc147dfba20a6737028c4a23b0ce44c5e4f227
Tarang Khanna <khanna17@purdue.edu>
2016-03-25 16:17:00 -0400

    gif+logout+ui touches

---
+++ Casper/RideSharing/MenuTableViewController.swift
@@ -0,0 +1,102 @@
+//
+//  MenuTableViewController.swift
+//  RideSharing
+//
+//  Created by Tarang khanna on 3/25/16.
+//  Copyright © 2016 Tarang khanna. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class MenuTableViewController: UITableViewController {
+    
+    @IBOutlet weak var logOutBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func logOut(sender: AnyObject) {
+        print("LOG OUT")
+        self.logOutBtn.backgroundColor = UIColor.grayColor()
+        UIView.animateWithDuration(1, animations: { () -> Void in
+            self.logOutBtn.backgroundColor = UIColor.grayColor()
+            }) { (success) -> Void in
+                self.logOutBtn.backgroundColor = UIColor.clearColor()
+        }
+        
+        PFUser.logOut()
+        performSegueWithIdentifier("signInAgain", sender: self)
+    }
+
+    // MARK: - Table view data source
+
+    /*
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Casper/RideSharing/driveViewController.swift
+++ Casper/RideSharing/driveViewController.swift
@@ -54,11 +54,15 @@ class driveViewController: UIViewController {
         let date = formatter.dateFromString(createdString)
         print(date)
         
+        
         Ride.postRide(fronTxt.text, arrivalPoint: toTxt.text, dateAndTime: dateView.date, price: priceTxt.text, description: desTxt.text, availability: true) { (success: Bool, error: NSError?) -> Void in
             if (success) {
                 print("success")
+                // segue
+                self.performSegueWithIdentifier("posted", sender: self)
             }
             if error != nil {
+                // popup error
                 print(error)
             }
         }
